09d289708e13fb6001772ad10d0e8266ef19ee1e
GrandaddyShmax <52707645+ClownOfMadness@users.noreply.github.com>
2023-04-17 00:50:32 +0300

    BSPMC237-11: Created AI page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,22 @@
 module.exports = {
   content: [
     './views/*.ejs',
-    './views/partials/*.ejs'
+    './views/partials/*.ejs',
+    './views/main/*.ejs',
+    './views/errors/*.ejs',
   ],
   theme: {
     extend: {
       colors: {
         mint: '#00ff90',
+        coffee: '#A9907E',
+        latte: '#F3DEBA',
+        pistachio: '#ABC4AA',
+        espresso: '#675D50',
       },
       dropShadow: {
-        lg: '0 4px 4px rgba(0, 255, 144, 0.1)',
-        xl: '0 5px 5px rgba(0, 255, 144, 0.3)',
+        lg: '0 4px 4px rgba(171, 196, 170, 0.1)',
+        xl: '0 5px 5px rgba(171, 196, 170, 0.3)',
       },
     },
   },
