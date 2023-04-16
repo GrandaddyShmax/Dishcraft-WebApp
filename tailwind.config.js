5147c70e613058b7078929bc6eb3bfa1b8f74b59
GrandaddyShmax <52707645+ClownOfMadness@users.noreply.github.com>
2023-04-16 20:14:04 +0300

    BSPMC237-11: created recipe database and made prototype homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: [
     './views/*.ejs',
+    './views/partials/*.ejs'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: '#00ff90',
+      },
+      dropShadow: {
+        lg: '0 4px 4px rgba(0, 255, 144, 0.1)',
+        xl: '0 5px 5px rgba(0, 255, 144, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
