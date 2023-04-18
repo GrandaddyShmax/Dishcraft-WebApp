3deb4c7d6e8c31885666d0c2d909601945c5f28c
GrandaddyShmax <52707645+ClownOfMadness@users.noreply.github.com>
2023-04-19 00:50:51 +0300

    Refined recipe management page

--- controllers/admin/manageRecipes.js
+++ controllers/admin/manageRecipes.js
@@ -3,20 +3,15 @@ const express = require("express");
 const router = express.Router();
 const { Recipe } = require("../../models/recipe");
 
-router.get("/home", async (req, res) => {
+router.get("/admin/managerecipes", async (req, res) => {
   const session = req.session;
   const recipes = await Recipe.fetchRecipes(session.filter || null, session.sort || null);
   res.render("template", {
-    pageTitle: "Dishcraft - Homepage",
-    page: "home",
+    pageTitle: "Dishcraft - Manage Recipes",
+    page: "A_manageRecipes",
     recipes: recipes,
     user: session.user || null,
   });
-});
-
-router.post("/home", async (req, res) => {
-  const session = req.session;
-  const smt = req.body.submit;
 });
 
 /*[ External access ]*/
